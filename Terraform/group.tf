516048c7e8f7e6c68f753b6d218153e591b95e8d
unknown <malfilatre.gauthier@gmail.com>
2023-01-24 11:24:12 +0100

    Création group.tf

--- Terraform/group.tf
+++ Terraform/group.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instance_minecraft" {
+resource "aws_instance" "instance_minecraft_group1" {
   ami           = "ami-0778521d914d23bc1" 
   instance_type = "t3.large"
   subnet_id = aws_subnet.public_subnet_minecraft.id
@@ -6,10 +6,4 @@ resource "aws_instance" "instance_minecraft" {
   key_name = aws_key_pair.keypair_minecraft.key_name
   security_groups = [aws_security_group.allow_ssh_minecraft.id]
 
- 
-
-  tags = {
-      Name = "terraform_instance"
-      build_by = "terraform"
-  }
 }
