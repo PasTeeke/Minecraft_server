046d4bd846facffa7072c7b74c5460ee2ba149a5
unknown <malfilatre.gauthier@gmail.com>
2023-01-24 10:00:18 +0100

    Rajout AZ

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -73,7 +73,7 @@ resource "aws_internet_gateway" "internet_gateway_minecraft" {
 resource "aws_eip" "eip_minecraft" {
     instance = aws_instance.instance_minecraft.id
     vpc = true
-
+    
  
 
     tags = {
@@ -98,6 +98,7 @@ resource "aws_instance" "instance_minecraft" {
   ami           = "ami-0778521d914d23bc1" 
   instance_type = "t3.large"
   subnet_id = aws_subnet.public_subnet_minecraft.id
+  availability_zone = "us-east-1a"
   key_name = aws_key_pair.keypair_minecraft.key_name
   security_groups = [aws_security_group.allow_ssh_minecraft.id]
 
